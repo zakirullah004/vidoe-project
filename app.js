f534bbe8f7f4e655ffc879dd7d48073b889b0617
zakirullah004 <zk743087@gmail.com>
2025-11-04 00:03:20 +0500

    js logic hogaya

---
+++ app.js
@@ -0,0 +1,15 @@
+let video = document.querySelector("#video")
+let playPause = document.querySelector("#playPause")
+
+playPause.addEventListener('click',function(){
+    // console.log(playPause.classList);
+    if(playPause.classList.contains("fa-play")){
+        video.pause();
+        playPause.classList.remove("fa-play")
+        playPause.classList.add("fa-pause")
+    }else{
+        video.play()
+        playPause.classList.remove('fa-pause')
+        playPause.classList.add('fa-play')
+    }
+})
